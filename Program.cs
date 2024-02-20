d5dd8168330d5d01737d92be51ea1afba75c138d
octavioduartejurczyszyn <tatoduarte9@gmail.com>
2024-02-20 13:29:33 +0100

    Primera subida

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Hacer un programa para mostrar los números del 1 al 10. No se debe realizar
+            // ningún pedido de datos.
+            int con = 0;
+            for (int x = 0; x < 10; x++)
+            {
+                con++;
+                Console.WriteLine(con);
+            }
+
+        }
+    }
+}
